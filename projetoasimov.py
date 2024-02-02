0146d5052eb223864ecc04fbf9485457e806c358
Lucas-Bellini <bellini.mp3@gmail.com>
2024-02-02 18:41:50 -0300

    new file:   pages/book_reviews.py 	modified:   projetoasimov.py

--- projetoasimov.py
+++ projetoasimov.py
@@ -23,3 +23,9 @@ max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
 df_books = df_top100_books[df_top100_books["book price"] <= max_price]
 df_books
 
+col1, col2 = st.columns(2)
+fig = px.bar(df_books["year of publication"].value_counts())
+fig2 = px.histogram(df_books["book price"])
+col1.plotly_chart(fig)
+col2.plotly_chart(fig2)
+
